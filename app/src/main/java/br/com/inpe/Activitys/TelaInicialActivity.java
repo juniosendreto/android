f9b328758ff0d56e7587b5e2b3f5100e5c11f5bf
juniosendreto <junio.sendreto@hotmail.com>
2015-12-18 11:28:02 -0200

    view

---
+++ app/src/main/java/br/com/inpe/Activitys/TelaInicialActivity.java
@@ -0,0 +1,15 @@
+package br.com.inpe.Activitys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.inpe.R;
+
+public class TelaInicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
